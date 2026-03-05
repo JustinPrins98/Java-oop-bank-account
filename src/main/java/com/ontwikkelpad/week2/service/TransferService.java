050a5c4d1f49a7f7121c5ae53e254a525831a02f
Justin Prins <justin.prins@interly.nl>
2026-03-05 08:56:58 +0100

    Add TransferService interface

---
+++ src/main/java/com/ontwikkelpad/week2/service/TransferService.java
@@ -0,0 +1,11 @@
+package com.ontwikkelpad.week2.service;
+
+import com.ontwikkelpad.week1.BankAccount;
+import java.math.BigDecimal;
+
+public class TransferService {
+
+    void Transfer(BankAccount source, BankAccount target, BigDecimal amount) {
+
+    }
+}
